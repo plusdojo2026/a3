b53f7eafcec2f4bfcf014707bc81497d65df981f
plusdojo2026-tanaka-daiki <tanaka-daiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 13:13:17 +0900

    改変

--- source/src/main/java/dto/dialog.java
+++ source/src/main/java/dto/dialog.java
@@ -5,17 +5,39 @@ public class dialog {
  	private String date;
 	private String contain;
 	private Integer userID;
+	
+	
+	public dialog(int dialogId, String date, String contain, Integer userID) {
+		super();
+		this.dialogId = dialogId;
+		this.date = date;
+		this.contain = contain;
+		this.userID = userID;
+	}
+	public int getDialogId() {
+		return dialogId;
+	}
+	public void setDialogId(int dialogId) {
+		this.dialogId = dialogId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContain() {
+		return contain;
+	}
+	public void setContain(String contain) {
+		this.contain = contain;
+	}
+	public Integer getUserID() {
+		return userID;
+	}
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
 
-		public int getDialogId() {
-				return dialogId;
-			}
- 		public String getDate() {
-				return date;
-			}
- 		public String getContain() {
-				return contain;
-			}
- 		public  Integer getUserId() {
-				return userID;
-			}
+		
 }
