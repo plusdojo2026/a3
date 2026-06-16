94dfd6dba18ddbf09ccb6be8714f4e634d734c8d
plusdojo2026-tanaka-daiki <tanaka-daiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:24:26 +0900

    更新

--- source/src/main/webapp/sql/subject.sql
+++ source/src/main/webapp/sql/subject.sql
@@ -1,8 +1,11 @@
+
+USE `classcare_db`;
+
 CREATE TABLE scores (
     id INT AUTO_INCREMENT PRIMARY KEY, -- データ番号
-    student_id VARCHAR(10) NOT NULL,   -- 生徒番号
-    test_year_month VARCHAR(7) NOT NULL, -- いつのテストか
-    math_score INT,                    -- 数学の点数
-    japanese_score INT,                -- 国語の点数
-    english_score INT                  -- 英語の点数
+    `student_id` VARCHAR(10) NOT NULL,   -- 生徒番号
+    `test_year_month` VARCHAR(7) NOT NULL, -- いつのテストか
+    `math_score` INT NULL,                    -- 数学の点数
+    `japanese_score` INT NULL,                -- 国語の点数
+    `english_score` INT NULL,                  -- 英語の点数
 );
