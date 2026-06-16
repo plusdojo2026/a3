488e828de7aab8dbfe8743df6d4629e20eda0938
plusdojo2026-hena <hai-na-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:28:49 +0900

    schedules修正_SQL(3)

--- source/src/main/webapp/sql/schedules.sql
+++ source/src/main/webapp/sql/schedules.sql
@@ -1,10 +1,10 @@
-CREATE TABLE 'schedules'(
-   'schedule_id' INT PRIMARY KEY,
-   'date' DATE,
-   'subject' VARCHAR(10),
-   'start_time' TIME,
-   'finish_time' TIME,
-   'type' VARCHAR(10),
-   'memo' VARCHAR(50),
-   'user_id' INT
+CREATE TABLE `schedules`(
+   `schedule_id` INT PRIMARY KEY,
+   `date` DATE,
+   `subject` VARCHAR(10),
+   `start_time` TIME,
+   `finish_time` TIME,
+   `type` VARCHAR(10),
+   `memo` VARCHAR(50),
+   `user_id` INT
 );
