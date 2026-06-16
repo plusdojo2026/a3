9f575d6b2276daa80545634fca4d6b42bb7eacc8
plusdojo2026-tanaka-daiki <tanaka-daiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 13:10:35 +0900

    dto

---
+++ source/src/main/java/dto/dialog.java
@@ -0,0 +1,21 @@
+package dto;
+
+public class dialog {
+	private int dialogId;
+ 	private String date;
+	private String contain;
+	private Integer userID;
+
+		public int getDialogId() {
+				return dialogId;
+			}
+ 		public String getDate() {
+				return date;
+			}
+ 		public String getContain() {
+				return contain;
+			}
+ 		public  Integer getUserId() {
+				return userID;
+			}
+}
