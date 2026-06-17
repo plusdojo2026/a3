df683ba92ff380e6960e5ecb10e65e1b8febaae3
plusdojo2026-miyauchia3 <miyauchi-ikono-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 10:14:01 +0900

    trouble sql

--- source/src/main/webapp/sql/trouble.sql
+++ source/src/main/webapp/sql/trouble.sql
@@ -11,3 +11,7 @@ CREATE TABLE `trouble` (
   `situation` VARCHAR(100) NOT NULL,
   FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`)
 );
+INSERT INTO trouble 
+(title, contents, members, user_id, situation)
+VALUES 
+('休み時間の口論', '休み時間に同じ班の生徒との口論があり、授業後に担任が聞き取りを行った。', '山田太郎', 1, '対応中');
