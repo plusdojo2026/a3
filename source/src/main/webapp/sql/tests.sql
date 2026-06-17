e6bf843ef2f32119430a832482d2b01e785c9e49
plusdojo2026-takahata <takahata-naoki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 10:00:37 +0900

    髙畑sqlサンプル挿入
    tests.sql , mental_tests.sqlにサンプルINSERTを追加

--- source/src/main/webapp/sql/tests.sql
+++ source/src/main/webapp/sql/tests.sql
@@ -15,3 +15,7 @@ CREATE TABLE `classcare_db`.`tests` (
   CONSTRAINT `fk_tests_score` FOREIGN KEY (`scores_id`) REFERENCES `classcare_db`.`scores` (`scores_id`),
   CONSTRAINT `fk_tests_subject` FOREIGN KEY (`subject_id`) REFERENCES `classcare_db`.`subjects` (`subject_id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+-- サンプルデータ
+INSERT INTO `tests` (`scores_id`, `test_date`,  `subject_id`, `user_id`)
+VALUES(1, '2026-06-17', 1, 1);
