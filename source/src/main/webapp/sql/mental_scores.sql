9d973ed45906734cfcefc5cb1964a02989e4beeb
misaki0731 <toraiwa-misaki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 10:11:15 +0900

    虎岩美咲希　SQL

--- source/src/main/webapp/sql/mental_scores.sql
+++ source/src/main/webapp/sql/mental_scores.sql
@@ -18,3 +18,11 @@ CREATE TABLE `mental_scores` (
   FOREIGN KEY (`mt_id`) REFERENCES `mental_tests`(`mt_id`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
 );
+
+INSERT INTO `mental_scores` (`score`, `status`, `mt_scores_memo`, `mt_id`, `user_id`)
+VALUES
+('85', 'valid', '良好', 1, 1),
+('40', 'valid', '注意', 2, 1),
+('100', 'valid', '非常に良い', 3, 1),
+('55', 'valid', '注意', 4, 1),
+('----', 'invalid', '未受験', 5, 1);
