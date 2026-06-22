024256608b5c350e5b30db7de463ef21f3ea805f
plusdojo2026-miyauchia3 <miyauchi-ikono-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 13:08:01 +0900

    サーブレット追加

---
+++ source/src/main/java/servlet/DeleteSubjectsServlet.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.SubjectsDao;
+
+/**
+ * Servlet implemention class DeleteSubjectsServlet
+ */
+
+@WebServlet("/DeleteSubjectsServlet")
+public class DeleteSubjectsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/LoginServlet");
+			return;
+		}
+
+		// パラメーター取得
+		String subjectIdString = request.getParameter("subject_id");
+		int subject_id = 0;
+
+		// nullのチェック
+		if (subjectIdString != null && !subjectIdString.isEmpty()) {
+
+			// intに変換
+			subject_id = Integer.parseInt(subjectIdString);
+		}
+
+		// Daoの呼び出し、booleanで結果を受け取る
+		SubjectsDao dao = new SubjectsDao();
+		boolean result = dao.delete(subject_id);
+
+		// 戻り値を返す
+		if (result) {
+			request.setAttribute("message", "科目を削除しました。");
+		} else {
+			request.setAttribute("message", "科目の削除に失敗しました。");
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/SubjectsMenu.jsp");
+		dispatcher.forward(request, response);
+
+	}
+}
---
+++ source/src/main/java/servlet/UpdateScoresServlet.java
@@ -0,0 +1,66 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ScoresDao;
+import dto.Scores;
+
+/**
+ * Servlet implemention class UpdateScoresServlet
+ */
+
+@WebServlet("/UpdateScoresServlet")
+
+public class UpdateScoresServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	// 更新時の画面表示
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		int scoresId = Integer.parseInt(request.getParameter("scores_id"));
+
+		ScoresDao dao = new ScoresDao();
+
+		Scores scores = dao.findById(scoresId);
+
+		request.setAttribute("scores", scores);
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Scores.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	// 更新の処理
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		int scoresId = Integer.parseInt(request.getParameter("scores_id"));
+		String score = request.getParameter("score");
+
+		ScoresDao dao = new ScoresDao();
+
+		Scores scores = new Scores();
+		scores.setScores_id(scoresId);
+		scores.setScore(score);
+
+		// updateの実行
+		boolean result = dao.update(scores, scoresId);
+
+		request.setAttribute("message", result ? "点数を更新しました。" : "点数の更新に失敗しました。");
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Scores.jsp");
+		dispatcher.forward(request, response);
+	}
+}
